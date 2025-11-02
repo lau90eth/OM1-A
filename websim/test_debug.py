b98b8f0826c305f1220248103f6fa7a17bd70079
lau90eth <lau90eth@gmail.com>
2025-11-02 17:04:22 +0100

    test(websim): add unit test for debug logging
    Closes #636

--- websim/test_debug.py
+++ websim/test_debug.py
@@ -1,4 +1,4 @@
-import unittest
+﻿import unittest
 from unittest.mock import patch
 from .debug import log_debug
 
@@ -6,7 +6,4 @@ class TestDebugLogging(unittest.TestCase):
     @patch('logging.getLogger')
     def test_log_debug(self, mock_logger):
         log_debug("test message")
-        mock_logger().debug.assert_called_with("[WebSim Debug] test message")
-
-if __name__ == '__main__':
-    unittest.main()
+        mock_logger.return_value.debug.assert_called_once_with("test message")
